56b89b5e9e9dfd3eb3539817eb6d670f4c85486f
nathan <luncefordna@coopdigity.com>
2025-08-30 18:30:47 -0600

    init

---
+++ 121/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _121;
+
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        
+        int lowest = 0;
+        int maxProfit = 0;
+
+        for(int i = 0; i < prices.Length; i++)
+        {
+            if(prices[i] < prices[lowest])
+            {
+                lowest = i;
+            }
+
+            if(prices[i] - prices[lowest] > maxProfit)
+            {
+                maxProfit = prices[i] - prices[lowest];
+            }
+        }
+
+        return maxProfit;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ 13/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _13;
+
+public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        Dictionary<char, int> numerals = new()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        int integer = 0;
+        for (int i = 0; i < s.Length; i++)
+            if (i > 0 && numerals[s[i]] > numerals[s[i - 1]])
+            {
+                integer += numerals[s[i]];
+                integer -= 2 * numerals[s[i - 1]];
+            }
+            else
+            {
+                integer += numerals[s[i]];
+            }
+
+        return integer;
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution solution = new();
+        string s = "MCMXCIV"; // 1994
+        // string s = "III";
+        int result = solution.RomanToInt(s);
+        Console.WriteLine(result);
+    }
+}
