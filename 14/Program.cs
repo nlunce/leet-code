fb0dd53fa108ebb38a4d1afb0c2cc0cfc13e156b
nathan <luncefordna@coopdigity.com>
2025-08-30 20:26:19 -0600

    14

---
+++ 14/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _14;
+
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        string longestPrefix = "";
+
+        string[] sorted = strs.OrderBy(s => s.Length).ToArray();
+        for (int letter = 0; letter < sorted[0].Length; letter++)
+        {
+            longestPrefix = sorted[0][..(letter + 1)];
+
+            for (int word = 0; word < sorted.Length; word++)
+                if (sorted[word].Length > letter && sorted[0][letter] != sorted[word][letter])
+                    return sorted[0][..letter];
+        }
+
+
+        return longestPrefix;
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution solution = new();
+        // string[] strs = new[] { "flower", "flow", "flight" };
+        string[] strs = new[] { "a" };
+
+        string result = solution.LongestCommonPrefix(strs);
+        Console.WriteLine(result);
+    }
+}
---
+++ 58/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _58;
+
+public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        string trimmed = s.TrimEnd();
+        char[] charArray = trimmed.ToCharArray();
+        Array.Reverse(charArray);
+
+        int counter = 0;
+        foreach (char c in charArray)
+        {
+            if (c == ' ') break;
+
+            counter++;
+        }
+
+        return counter;
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
