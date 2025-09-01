22b6fb23f96a361b40e79bdba1ea55ba2b21dc74
nathan <luncefordna@coopdigity.com>
2025-09-01 16:58:33 -0600

    28

---
+++ 28/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _28;
+
+public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        int j = 0;
+        for (int i = 0; i < haystack.Length; i++)
+        {
+            if (haystack[i] == needle[j])
+            {
+                j++;
+            }
+            else
+            {
+                i = i - j;
+                j = 0;
+            }
+
+            if (j == needle.Length) return i - j + 1;
+        }
+
+        return -1;
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution solution = new();
+        int result = solution.StrStr("mississippi", "issip");
+
+        Console.WriteLine(result);
+    }
+}
