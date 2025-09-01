526c55aca0875cd3c59fd67bd068e7570c459214
nathan <luncefordna@coopdigity.com>
2025-09-01 17:37:11 -0600

    392

---
+++ 392/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _392;
+
+public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        if (s.Length == 0) return true;
+
+        if (t.Length == 0) return false;
+
+        int j = 0;
+        for (int i = 0; i < t.Length; i++)
+            if (t[i] == s[j])
+            {
+                j++;
+
+                if (j == s.Length) return true;
+            }
+
+        return false;
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution solution = new();
+        bool result = solution.IsSubsequence("abc", "ahbgdc");
+        Console.WriteLine(result);
+    }
+}
