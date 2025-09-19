bf0452637c8898cad1a141727f35abc6007c0387
nathan <luncefordna@coopdigity.com>
2025-09-19 16:59:36 -0600

    1

---
+++ 1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _1;
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> map = new();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (map.ContainsKey(target - nums[i])) return [map[target - nums[i]], i];
+
+            map[nums[i]] = i;
+        }
+
+        return [];
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
