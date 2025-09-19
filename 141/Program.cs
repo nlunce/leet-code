129b3de89bc265af3e3d3002cb0ed713036a6dc9
nathan <luncefordna@coopdigity.com>
2025-09-18 18:02:36 -0600

    205

---
+++ 141/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _141;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ 205/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _205;
+
+public class Solution
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+        Dictionary<char, char> map = new();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!map.ContainsKey(s[i]) && !map.ContainsValue(t[i])) map.Add(s[i], t[i]);
+
+
+            if (map.ContainsKey(s[i]) && map[s[i]] == t[i]) continue;
+
+            return false;
+        }
+
+        return true;
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
