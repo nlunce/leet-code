76be8ae05534719523402c8a8fd971835ee39fb5
nathan <luncefordna@coopdigity.com>
2025-09-19 13:20:47 -0600

    290

---
+++ 290/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _290;
+
+public class Solution
+{
+    public bool WordPattern(string pattern, string s)
+    {
+        Dictionary<char, string> map = new();
+        string[] words = s.Split(' ');
+
+        if (pattern.Length != words.Length) return false;
+
+        for (int i = 0; i < pattern.Length; i++)
+        {
+            if (!map.ContainsKey(pattern[i]) && !map.ContainsValue(words[i])) map.Add(pattern[i], words[i]);
+
+
+            if (map.ContainsKey(pattern[i]) && map[pattern[i]] == words[i]) continue;
+
+            return false;
+        }
+
+        return true;
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
