1b343f5b985976d2c2a7d384c1c35f6cc5fdd0a1
nathan <luncefordna@coopdigity.com>
2025-09-22 14:23:32 -0600

    219

---
+++ 219/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _219;
+
+public class Solution
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        Dictionary<int, int> map = new();
+        for (int i = 0; i < nums.Length; i++)
+            if (!map.ContainsKey(nums[i]))
+            {
+                map.Add(nums[i], i);
+            }
+            else
+            {
+                int j = map[nums[i]];
+                if (Math.Abs(i - j) <= k) return true;
+                map[nums[i]] = i;
+            }
+
+        return false;
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution sol = new();
+        Console.WriteLine(sol.ContainsNearbyDuplicate(new[] { 1, 2, 3, 1, 2, 3 }, 2)); // false
+    }
+}
