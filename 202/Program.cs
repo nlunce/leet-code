82d80fdb55e75420d1dd508d01daf18bfbf5d591
nathan <luncefordna@coopdigity.com>
2025-09-22 13:14:14 -0600

    202

---
+++ 202/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _202;
+
+public class Solution
+{
+    public bool IsHappy(int n)
+    {
+        Dictionary<int, int> map = new();
+        while (n != 1)
+        {
+            List<int> listOfInts = new();
+            int num = n;
+            while (num > 0)
+            {
+                listOfInts.Add(num % 10);
+                num = num / 10;
+            }
+
+            int sum = 0;
+
+            foreach (int i in listOfInts) sum += i * i;
+
+            if (map.ContainsKey(sum)) return false;
+
+            map.Add(n, sum);
+
+            n = sum;
+        }
+
+        return true;
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution solution = new();
+        Console.WriteLine(solution.IsHappy(19)); // true
+    }
+}
