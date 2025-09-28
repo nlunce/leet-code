4055ae291acf525b8db4a26c62db1f4483cd12c1
nathan <luncefordna@coopdigity.com>
2025-09-27 18:50:19 -0600

    20

---
+++ 20/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _20;
+
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        if (s.Length < 2) return false;
+
+        Stack<char> stack = new();
+        Dictionary<char, char> map = new();
+        map.Add('(', ')');
+        map.Add('[', ']');
+        map.Add('{', '}');
+
+        if (map.ContainsKey(s[0]))
+            stack.Push(s[0]);
+        else
+            return false;
+
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (map.ContainsKey(s[i]))
+            {
+                stack.Push(s[i]);
+                continue;
+            }
+
+            if (stack.Count == 0) return false;
+
+            if (map[stack.Peek()] == s[i])
+                stack.Pop();
+            else
+                return false;
+        }
+
+        if (stack.Count() == 0) return true;
+        return false;
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution solution = new();
+        Console.WriteLine(solution.IsValid("()")); // true
+    }
+}
