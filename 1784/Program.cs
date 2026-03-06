5ad77b2e50c976178708007ca796f78b7ab0015e
nathan <luncefordna@coopdigity.com>
2026-03-06 14:57:31 -0700

    1784

---
+++ 1784/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _1784;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string s = "110";
+        Solution solution = new();
+        bool result = solution.CheckOnesSegment(s);
+        Console.WriteLine(result);
+    }
+}
+
+public class Solution
+{
+    public bool CheckOnesSegment(string s)
+    {
+    }
+}
