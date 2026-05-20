7b2fe4989134e4cd760e16e67f55b99957a8a723
nathan <luncefordna@coopdigity.com>
2026-05-20 14:19:55 -0600

    21

---
+++ 21/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _21;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ListNode list1 = new(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new(1, new ListNode(3, new ListNode(4)));
+
+        Solution solution = new();
+
+        ListNode result = solution.MergeTwoLists(list1, list2);
+        ListNode current = result;
+        while (current != null)
+        {
+            Console.Write(current.val);
+            if (current.next != null) Console.Write(" -> ");
+            current = current.next;
+        }
+
+        Console.WriteLine();
+    }
+}
+
+public class ListNode(int val = 0, ListNode next = null)
+{
+    public ListNode next = next;
+    public int val = val;
+}
+
+public class Solution
+{
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode dummy = new();
+        ListNode tail = dummy;
+
+        while (list1 != null && list2 != null)
+            if (list1.val < list2.val)
+            {
+                tail.next = new ListNode(list1.val);
+                list1 = list1.next;
+                tail = tail.next;
+            }
+            else
+            {
+                tail.next = new ListNode(list2.val);
+                list2 = list2.next;
+                tail = tail.next;
+            }
+
+        tail.next = list1 ?? list2;
+        return dummy.next;
+    }
+}
---
+++ 67/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _67;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public class Solution
+{
+    public string AddBinary(string a, string b)
+    {
+        int maxLen = Math.Max(a.Length, b.Length);
+        a = a.PadLeft(maxLen, '0');
+        b = b.PadLeft(maxLen, '0');
+
+        char[] result = new char[maxLen];
+        char carry = '0';
+
+        for (int i = maxLen - 1; i > 0; i--)
+        {
+            if (a[i] == '0' && b[i] == '0' && carry == '0')
+            {
+                result[i] = '0';
+                carry = '0';
+            }
+
+            if (a[i] == '1' && b[i] == '0' && carry == '0')
+            {
+                result[i] = '1';
+                carry = '0';
+            }
+
+            if (a[i] == '0' && b[i] == '1' && carry == '0')
+            {
+                result[i] = '1';
+                carry = '0';
+            }
+
+
+            if (a[i] == '0' && b[i] == '0' && carry == '1')
+            {
+                result[i] = '1';
+                carry = '0';
+            }
+
+            if (a[i] == '1' && b[i] == '1' && carry == '0')
+            {
+                result[i] = '0';
+                carry = '1';
+            }
+
+            if (a[i] == '1' && b[i] == '1' && carry == '1')
+            {
+                result[i] = '1';
+                carry = '1';
+            }
+        }
+
+        return new string(result);
+    }
+}
