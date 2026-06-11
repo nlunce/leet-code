c85a188eed305ac45844e49b1be62e7c07817790
nathan <luncefordna@coopdigity.com>
2026-06-11 15:38:53 -0600

    1768

---
+++ 1768/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace _1768;
+
+public class Solution
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        StringBuilder result = new();
+        int min = Math.Min(word1.Length, word2.Length);
+
+        for (int i = 0; i < min; i++)
+        {
+            result.Append(word1[i]);
+            result.Append(word2[i]);
+        }
+
+        result.Append(word1, min, word1.Length - min);
+        result.Append(word2, min, word2.Length - min);
+        return result.ToString();
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string word1 = "abc";
+        string word2 = "pq";
+        Solution solution = new();
+        string result = solution.MergeAlternately(word1, word2);
+        Console.WriteLine(result);
+    }
+}
