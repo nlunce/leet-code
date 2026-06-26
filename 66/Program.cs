493a7adf98901a683de79d15a6d99eb1c9ae5ee8
nathan <luncefordna@coopdigity.com>
2026-06-26 14:30:32 -0600

    66

---
+++ 66/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _66;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public class Solution
+{
+    public int[] PlusOne(int[] digits)
+    {
+        for (int i = digits.Length - 1; i >= 0; i--)
+            if (digits[i] == 9)
+            {
+                digits[i] = 0;
+            }
+            else
+            {
+                digits[i] = digits[i] + 1;
+                return digits;
+            }
+
+        return new[] { 1 }.Concat(digits).ToArray();
+    }
+}
