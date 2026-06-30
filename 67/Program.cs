fb12cca851d9f494ee224ed94499c1553b9e24ca
nathan <luncefordna@coopdigity.com>
2026-06-30 12:56:26 -0600

    67

--- 67/Program.cs
+++ 67/Program.cs
@@ -12,53 +12,48 @@ public class Solution
 {
     public string AddBinary(string a, string b)
     {
-        int maxLen = Math.Max(a.Length, b.Length);
-        a = a.PadLeft(maxLen, '0');
-        b = b.PadLeft(maxLen, '0');
+        int x = a.Length - 1;
+        int y = b.Length - 1;
 
-        char[] result = new char[maxLen];
-        char carry = '0';
+        List<char> result = new();
 
-        for (int i = maxLen - 1; i > 0; i--)
+        int carry = 0;
+
+
+        while (x >= 0 || y >= 0 || carry != 0)
         {
-            if (a[i] == '0' && b[i] == '0' && carry == '0')
+            if ((x >= 0 ? a[x] - '0' : 0) + (y >= 0 ? b[y] - '0' : 0) + carry == 3)
             {
-                result[i] = '0';
-                carry = '0';
+                carry = 1;
+
+                result.Add('1');
+            }
+            else if ((x >= 0 ? a[x] - '0' : 0) + (y >= 0 ? b[y] - '0' : 0) + carry == 2)
+            {
+                carry = 1;
+
+                result.Add('0');
+            }
+            else if ((x >= 0 ? a[x] - '0' : 0) + (y >= 0 ? b[y] - '0' : 0) + carry == 1)
+            {
+                carry = 0;
+
+                result.Add('1');
+            }
+            else
+            {
+                carry = 0;
+
+                result.Add('0');
             }
 
-            if (a[i] == '1' && b[i] == '0' && carry == '0')
-            {
-                result[i] = '1';
-                carry = '0';
-            }
-
-            if (a[i] == '0' && b[i] == '1' && carry == '0')
-            {
-                result[i] = '1';
-                carry = '0';
-            }
-
-
-            if (a[i] == '0' && b[i] == '0' && carry == '1')
-            {
-                result[i] = '1';
-                carry = '0';
-            }
-
-            if (a[i] == '1' && b[i] == '1' && carry == '0')
-            {
-                result[i] = '0';
-                carry = '1';
-            }
-
-            if (a[i] == '1' && b[i] == '1' && carry == '1')
-            {
-                result[i] = '1';
-                carry = '1';
-            }
+            x = x - 1;
+            y = y - 1;
         }
 
-        return new string(result);
+
+        result.Reverse();
+        string sum = new(result.ToArray());
+        return sum;
     }
 }
