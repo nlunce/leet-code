151760a4fb71186c0f2f8fa98bfbd51bd1e16f2f
nathan <luncefordna@coopdigity.com>
2026-07-01 15:23:49 -0600

    70

---
+++ 70/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _70;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public class Solution
+{
+    public int ClimbStairs(int n)
+    {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for (int i = 2; i < dp.Length; i++) dp[i] = dp[i - 1] + dp[i - 2];
+
+        return dp[^1];
+    }
+}
